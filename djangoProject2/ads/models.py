053d94532632d432ddcb11d5e13b40be39b90c02
Drezz11 <109616201+Drezz11@users.noreply.github.com>
2023-02-18 20:03:19 +0400

    Файлы загружен

---
+++ djangoProject2/ads/models.py
@@ -0,0 +1,33 @@
+
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+
+    class Mera:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+class Ad(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    # address = models.CharField(max_length=300)
+    # locations = models.ManyToManyField("Location")
+    is_published = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True, upload_to="pictures")
+
+    class Mera:
+        verbose_name = "Объявления"
+        verbose_name_plural = "Объявления"
+
+    def __str__(self):
+        return self.name
